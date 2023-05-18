ab4316740a2ba06082a5a1568ae8b086e849c306
Jeongseop-Shin <tjql7757@gmail.com>
2023-05-19 00:59:33 +0900

    메인메뉴 기능 구현

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        TypingGame game = new TypingGame();
-        game.setVisible(true);
+        MainMenuFrame mainMenu = new MainMenuFrame();
+        mainMenu.setVisible(true);
     }
 }
